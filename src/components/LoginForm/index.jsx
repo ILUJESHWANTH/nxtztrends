b1aafb489903520495b1793ad0a0d9f1e54d2cb5
ILU JESHWANTH <ilujeshwanth31@gmail.com>
2026-02-10 22:11:32 +0530

    initial commit

---
+++ src/components/LoginForm/index.jsx
@@ -0,0 +1,123 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import {Redirect} from 'react-router-dom'
+import './index.css'
+
+class LoginForm extends Component {
+  state = {
+    username: '',
+    password: '',
+    showSubmitError: false,
+    errorMsg: '',
+  }
+
+  onSubmitSuccess = jwtToken => {
+    Cookies.set('jwt_token', jwtToken, {expires: 30})
+    const {history} = this.props
+    history.replace('/')
+  }
+
+  onSubmitFailure = errorMsg => {
+    this.setState({
+      showSubmitError: true,
+      errorMsg,
+    })
+  }
+
+  submitForm = async event => {
+    event.preventDefault()
+
+    const {username, password} = this.state
+    const userDetails = {username, password}
+
+    const response = await fetch('https://apis.ccbp.in/login', {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    })
+
+    const data = await response.json()
+
+    if (response.ok) {
+      this.onSubmitSuccess(data.jwt_token)
+    } else {
+      this.onSubmitFailure(data.error_msg)
+    }
+  }
+
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  render() {
+    const {username, password, showSubmitError, errorMsg} = this.state
+
+    const jwtToken = Cookies.get('jwt_token')
+
+    // If already logged in → go to Home
+    if (jwtToken !== undefined) {
+      return <Redirect to="/" />
+    }
+
+    return (
+      <div className="login-form-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          className="login-website-logo-mobile-image"
+          alt="website logo"
+        />
+
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png"
+          className="login-image"
+          alt="website login"
+        />
+
+        <form className="form-container" onSubmit={this.submitForm}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            className="login-website-logo-desktop-image"
+            alt="website logo"
+          />
+
+          <div className="input-container">
+            <label className="input-label" htmlFor="username">
+              USERNAME
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="username-input-filed"
+              value={username}
+              onChange={this.onChangeUsername}
+            />
+          </div>
+
+          <div className="input-container">
+            <label className="input-label" htmlFor="password">
+              PASSWORD
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="password-input-filed"
+              value={password}
+              onChange={this.onChangePassword}
+            />
+          </div>
+
+          <button type="submit" className="login-button">
+            Login
+          </button>
+
+          {showSubmitError && <p className="error-message">*{errorMsg}</p>}
+        </form>
+      </div>
+    )
+  }
+}
+
+export default LoginForm
